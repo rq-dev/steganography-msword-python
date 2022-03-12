0f0854f81a4c99eb45656835452ec938385e6d77
yromkaq@mail.ru <yromkaq@mail.ru>
2022-03-12 17:13:05 +0500

    methods

--- main.py
+++ main.py
@@ -2,46 +2,45 @@ import docx
 from docx.shared import RGBColor
 import string
 
-document = docx.Document('./test.docx')
-
+doc = docx.Document('./test.docx')
 f = open('./text.txt', 'r')
 to_hide_text = "".join(f.readlines())
+color = RGBColor(8, 92, 168)
 
 
 def get_letters_to_hide(t):
     return list("".join(t.translate(str.maketrans('', '', string.punctuation)).split()))
 
 
-print(get_letters_to_hide(to_hide_text))
+def hide_text(document, text_to_hide, filename):
+    letters_to_hide = get_letters_to_hide(text_to_hide)
+    for para in document.paragraphs:
+        text = para.text.split()
+        para.text = ''
+        for word in text:
+            if len(letters_to_hide) == 0:
+                para.add_run(word + " ")
+                continue
+            if letters_to_hide[0].lower() in word.lower():
+                words_letters = list(word)
+                # print(words_letters)
+                is_marked = False
+                for l in words_letters:
+                    if not is_marked and l.lower() == letters_to_hide[0].lower():
+                        isLower = l.islower()
+                        if not isLower:
+                            l = l.upper()
+                        del letters_to_hide[0]
+                        # print(letters_to_hide)
+                        para.add_run(l).font.color.rgb = color
+                        is_marked = True
+                    else:
+                        para.add_run(l)
+                para.add_run(" ")
+            else:
+                para.add_run(word + " ")
 
-letters_to_hide = get_letters_to_hide(to_hide_text)
-
-for para in document.paragraphs:
-    text = para.text.split()
-    para.text = ''
-    for word in text:
-        if len(letters_to_hide) == 0:
-            para.add_run(word + " ")
-            continue
-        if letters_to_hide[0].lower() in word.lower():
-            words_letters = list(word)
-            # print(words_letters)
-            is_marked = False
-            for l in words_letters:
-                if not is_marked and l.lower() == letters_to_hide[0].lower():
-                    isLower = l.islower()
-                    if not isLower:
-                        l = l.upper()
-                    del letters_to_hide[0]
-                    # print(letters_to_hide)
-                    para.add_run(l).font.color.rgb = RGBColor(8, 92, 168)
-                    is_marked = True
-                else:
-                    para.add_run(l)
-            para.add_run(" ")
-        else:
-            para.add_run(word + " ")
+            document.save(filename)
 
 
-document.save('./test.docx')
-
+hide_text(doc, to_hide_text, 'text.docx')
